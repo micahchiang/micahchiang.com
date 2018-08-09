4ab75fc645c3ea407435597abd594cf5fd72bb48
Micah Chiang <micahkchiang@gmail.com>
2018-08-08 21:18:34 -0700

    initial commit

---
+++ components/EntriesComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <ul>
+        <li v-for="entry in entries" :key="entry.id">
+            {{entry.title}}
+        </li>
+    </ul>
+</template>
+
+<script>
+    import {mapState} from 'vuex';
+
+    export default {
+        name: 'EntriesComponent',
+        computed: mapState([
+            "entries"
+        ])
+    }    
+</script>
+
+<style>
+</style>
+
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <entries-component></entries-component>
+</template>
+
+<script>
+import EntriesComponent from '~/components/EntriesComponent';
+
+export default {
+  components: {
+    EntriesComponent
+  },
+  async fetch( {store} ) {
+    await store.dispatch('LOAD_ITEMS', 'posts');
+  }
+}
+</script>
+
+<style>
+</style>
+
