eb83070cd6f9aed3f2863e01d5acaea77001ae23
Micah Chiang <micahkchiang@gmail.com>
2022-01-22 12:10:37 -0600

    fix gitignore

---
+++ src/pages/index.js
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import { Link, useStaticQuery, graphql } from 'gatsby';
+import Layout from '../components/layout.js';
+
+export default function IndexPage() {
+  const data = useStaticQuery(graphql`
+    query GetBlogPosts {
+      allSanityPost(limit: 10, sort: { order: DESC, fields: publishedAt }) {
+        nodes {
+          title
+          gatsbyPath(filePath: "/post/{SanityPost.slug__current}")
+          id
+          publishedAt(fromNow: true)
+        }
+      }
+    }
+  `);
+  const posts = data.allSanityPost.nodes;
+  return (
+    <>
+      <Layout>
+        <h1>On the web and other things</h1>
+
+        <h2>Recent Posts</h2>
+        <ul>
+          {posts.map((post) => (
+            <li key={post.id}>
+              <Link to={post.gatsbyPath}>{post.title}</Link>{' '}
+              <small>published {post.publishedAt}</small>
+            </li>
+          ))}
+        </ul>
+      </Layout>
+    </>
+  );
+}
